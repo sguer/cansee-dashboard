4ce682a9fdeca8f8121a5297679f9e864ce4ac8e
sguer <44205381+sguer@users.noreply.github.com>
2025-09-16 19:38:58 -0400

    Initial commit of CANSEE dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import CANSEEDashboardv3 from './CANSEEDashboardv3';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <CANSEEDashboardv3 />;
 }
 
 export default App;
